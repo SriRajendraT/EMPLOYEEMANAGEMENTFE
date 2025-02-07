6a923e895cd255cd6a3fbace278c599a6232fcb8
Sri Rajendra Turaga <Rajendratm120@gmail.com>
2025-02-07 19:30:45 +0530

    changes

--- src/app/Services/Storage-services/StorageService.ts
+++ src/app/Services/Storage-services/StorageService.ts
@@ -2,19 +2,19 @@ import { Injectable } from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
-  })
-  export class StorageService {
-  
-    constructor() { }
-    get(key: string): string | null {
-      return localStorage.getItem(key);
-    }
-  
-    set(key: string, value: string): void {
-      localStorage.setItem(key, value);
-    }
-  
-    remove(key: string): void {
-      localStorage.removeItem(key);
-    }
+})
+export class StorageService {
+
+  constructor() { }
+  get(key: string): string | null {
+    return localStorage.getItem(key);
   }
+
+  set(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  remove(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
